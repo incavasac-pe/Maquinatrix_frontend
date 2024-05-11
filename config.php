591e16d52e1ccf3630a42b0e45b81e5ae2a6afee
Norbelys Naguanagua <norbelysnaguanagua21@gmail.com>
2024-05-10 21:57:05 -0500

    credenciales api original

--- config.php
+++ config.php
@@ -1,16 +1,4 @@
 <?php 
-
-//Include Google Client Library for PHP autoload file
-require_once 'vendor/autoload.php';
-
-//Make object of Google API Client for call Google API
-$google_client = new Google_Client();
  
-$google_client->setRedirectUri('https://maquinatrix.com');
-
-// to get the email and profile 
-$google_client->addScope('email');
-
-$google_client->addScope('profile');
 
 ?> 
