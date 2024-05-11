ed47c4746b77edf02802e7f77ec5ad05b94f57bf
Norbelys Naguanagua <norbelysnaguanagua21@gmail.com>
2024-05-10 21:56:47 -0500

    credenciales api original

--- config.php
+++ config.php
@@ -5,13 +5,7 @@ require_once 'vendor/autoload.php';
 
 //Make object of Google API Client for call Google API
 $google_client = new Google_Client();
-
-//Set the OAuth 2.0 Client ID $google_client->setClientId('xxx');
-
-//Set the OAuth 2.0 Client Secret key 
-$google_client->setClientSecret('xxxx');
-
-//Set the OAuth 2.0 Redirect URI
+ 
 $google_client->setRedirectUri('https://maquinatrix.com');
 
 // to get the email and profile 
