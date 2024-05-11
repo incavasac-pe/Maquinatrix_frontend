e7f38aaff0ced415c532b5d975c7377688d775da
Norbelys Naguanagua <norbelysnaguanagua21@gmail.com>
2024-05-10 21:56:24 -0500

    credenciales api original

--- config.php
+++ config.php
@@ -6,12 +6,9 @@ require_once 'vendor/autoload.php';
 //Make object of Google API Client for call Google API
 $google_client = new Google_Client();
 
-//Set the OAuth 2.0 Client ID
-//$google_client->setClientId('257422679122-3c6egr7hrl34p7hik6emckf99rqgbn8f.apps.googleusercontent.com');
-$google_client->setClientId('xxx');
+//Set the OAuth 2.0 Client ID $google_client->setClientId('xxx');
 
-//Set the OAuth 2.0 Client Secret key
-//$google_client->setClientSecret('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');
+//Set the OAuth 2.0 Client Secret key 
 $google_client->setClientSecret('xxxx');
 
 //Set the OAuth 2.0 Redirect URI
